fa7de5e86b41f5955453e5e2d5f9aabbd530d9f0
Constadin <114903866+Constadin@users.noreply.github.com>
2024-01-16 23:34:43 +0200

    Add files via upload

---
+++ QR_make_stores/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZXing;
+using ZXing.Common;
+
+
+namespace QR_make_stores
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            QRCodeGenerator  generator = new QRCodeGenerator();
+            string NAME = "First Name";
+            string LASTNAME = "Last Name\n";
+            string idiotita = "Developer, Application Technician ( Web Developer & Video Games) & Electronic Technician\n";
+            string viografiko = "https://www.facebook.com";
+            string vgithub = "https://www.instagram.com/";
+
+            Bitmap qrCodeBitmap = generator.GenerateQRCode(NAME, LASTNAME, idiotita, viografiko, vgithub);
+            // Save the QR code as an image file
+            string outputPath = "../../Person.png"; // Output file path
+            qrCodeBitmap.Save(outputPath);
+
+            Console.WriteLine("QR code generated and saved as qrcode.png.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ QR_make_stores/QRCodeGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZXing;
+using ZXing.Common;
+using System.Drawing;
+using ZXing.QrCode;
+
+
+namespace QR_make_stores
+{
+    public class QRCodeGenerator
+    {
+        public Bitmap GenerateQRCode (string NAME ,string LASTNAME, string idiotita, string viografiko, string vgithub)
+        {
+            string content =($"{NAME}\n{LASTNAME}\n{idiotita}\nlinkedin: {viografiko}\n\ngithub: {vgithub}");
+
+            BarcodeWriter writer = new BarcodeWriter
+            {
+                Format = BarcodeFormat.QR_CODE,
+                Options = new QrCodeEncodingOptions
+                {
+                    Width = 600, Height = 600, Margin = 0 ,
+                    CharacterSet = "UTF-8"
+                }
+
+            };
+
+            return writer.Write(content);
+        }
+    }
+}
